093f73b38109575402b4ab9bddeaabde775aa788
vishwanath s patil <126999436+vishwanath7798@users.noreply.github.com>
2024-01-07 10:56:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,35 @@
 import React from "react";
-import "../styles/App.css";
-import { BrowserRouter as Router,Link, Route, Switch, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Shapes from "./Shapes";
 import Image from "./Image";
 
-
-
-
-export default function App() {
+const App = () => {
   return (
-    
-    <div id="home">
-      <nav className="navbar navbar-light">
-      <ul className="nav navbar-nav">
-          <li>
-            { /*Home */}
-          </li>
-          <li>
-            {/*Shapes*/}
-          </li>
-          <li>
-            {/*Image*/}
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <Router>
+      <div id="home">
+        <nav className="navbar navbar-light">
+          <ul className="nav navbar-nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/shapes">Shapes</Link>
+            </li>
+            <li>
+              <Link to="/image">Image</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       
+      <Switch>
+        <Route path="/shapes" component={Shapes} />
+        <Route path="/image" component={Image} />
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
   );
-}
-// export default App;
+};
+
+export default App;
