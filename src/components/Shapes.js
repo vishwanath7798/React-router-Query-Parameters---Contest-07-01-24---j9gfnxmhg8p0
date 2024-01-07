03ad2695c935c741e1ca9088e2958a57c3e1b48a
vishwanath s patil <126999436+vishwanath7798@users.noreply.github.com>
2024-01-07 10:56:51 +0530

    Update Shapes.js

--- src/components/Shapes.js
+++ src/components/Shapes.js
@@ -1,60 +1,32 @@
-import React from 'react'
-import "../styles/Shapes.css";
-import { Link, useLocation } from 'react-router-dom'
+import React from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import Child from './Child';
 
-  
- export default function Shapes() {
-   
-    
-  
-    // <Home />
-    return (
-      <div id='shapesdiv'>
-        
-          <h2>Shapes</h2>
-          <ul className="navbar-shapes">
-          <li>
-            <Link to="/shapes?shape=square&color=Blue">Blue Square</Link>
-          </li>
-          <li>
-            <Link to="/shapes?shape=square&color=Red">Red Square</Link>
-          </li>
-          <li>
-            <Link to="/shapes?shape=circle&color=Blue">Blue Circle</Link>
-          </li>
-          <li>
-            <Link to="/shapes?shape=circle&color=Red">Red Circle</Link>
-          </li>
-        </ul>
-  
-          
-        
-      </div>
-    );
-  }
-//   DO NOT touch CHILD function
-  function Child({ shape,color }) {
-    
-    if (shape === "circle"){
-        return (
-          <div id ="circle"className = "divcircle" style={{backgroundColor: color}}>
-            <h2>{shape}</h2>
-          </div>
-    
-        );
-      }
-      else if (shape === "square"){
-        return (
-          <div id ="square"className = "divsquare" style={{backgroundColor: color}}>
-            <h2>{shape}</h2>
-          </div>
-    
-        );
-      } 
-      else return(
-        <div id = "empty"className="empty"></div>
-      )
-      
-  }
+const Shapes = () => {
+  const location = useLocation();
+  const { shape, color } = new URLSearchParams(location.search);
 
-// export default Shapes
+  return (
+    <div id='shapesdiv'>
+      <h2>Shapes</h2>
+      <ul className="navbar-shapes">
+        <li>
+          <Link to="/shapes?shape=square&color=Blue">Blue Square</Link>
+        </li>
+        <li>
+          <Link to="/shapes?shape=square&color=Red">Red Square</Link>
+        </li>
+        <li>
+          <Link to="/shapes?shape=circle&color=Blue">Blue Circle</Link>
+        </li>
+        <li>
+          <Link to="/shapes?shape=circle&color=Red">Red Circle</Link>
+        </li>
+      </ul>
+
+      <Child shape={shape} color={color} />
+    </div>
+  );
+};
+
+export default Shapes;
